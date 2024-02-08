9c07f51f8e14d5a0be257a13f905649172e1bd63
Khalid Bouychou <khbouych@e3r3p18.1337.ma>
2024-02-08 17:33:25 +0100

    trainning python

---
+++ input.py
@@ -0,0 +1,2 @@
+input = input("age :")
+print (type(input))
---
+++ numbers.py
@@ -0,0 +1,16 @@
+import math
+
+print (8 + 5)
+print (8 / 5)
+print (8 // 5)#res integer part
+print (8 - 5)
+print (8 * 5)
+print (10 % 3)
+
+print("**"*20)
+
+x = 6.4
+
+math.ceil(x)
+print(round(x))
+print(abs(x))
